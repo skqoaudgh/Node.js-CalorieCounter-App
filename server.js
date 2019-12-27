1df24b6476c355b7e2b910556e3234ebefd025b7
myungho <skqoaudgh2@hanmail.net>
2019-12-27 15:42:44 +0900

    음식 칼로리 정보가 담긴 xml을 파싱하는 기능 추가

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const fs = require('fs');
+const xml2js = require('xml2js');
 
 const app = express();
+
+let foodInfo = [];
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -10,4 +14,13 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.listen(3000, console.log('Server is running on port 3000'));
+app.listen(3000, () => {
+    const parser = new xml2js.Parser();
+    fs.readFile(__dirname + '/resource/Food_Display_Table.xml', function(err, data) {
+        parser.parseString(data, function (err, result) {
+            foodInfo = result.Food_Display_Table.Food_Display_Row;
+        });
+    });
+
+    console.log('Server is running on port 3000');
+});
