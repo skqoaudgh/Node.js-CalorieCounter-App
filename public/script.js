a55372e1747389b49dd776d87e5ad857451ec1cc
myungho <skqoaudgh2@hanmail.net>
2019-12-28 17:15:16 +0900

    최초에는 항목 25개만 보여주고, 버튼을 통해 다음 항목을 볼 수 있도록

--- public/script.js
+++ public/script.js
@@ -1,7 +1,16 @@
 const clearBtn = document.getElementById('clear');
+const moreBtn = document.getElementById('more');
 
 if(clearBtn) {
     clearBtn.addEventListener('click', (e) => {
         window.location.href = "http://localhost:3000"; 
     });
 }
+
+moreBtn.addEventListener('click', (e) => {
+    const hiddenList = document.getElementsByClassName('hidden');
+    const max = hiddenList.length<5?hiddenList.length:5;
+    for(let i=0; i<max; i++) {
+        hiddenList[0].classList.remove('hidden');
+    }
+});
